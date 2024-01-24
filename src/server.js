ee3935eaa68c0f9b0c69e9c8e364b01ff778a729
João P. Meneguesso <jpm83633@gmail.com>
2024-01-24 10:54:23 -0300

    Adicionando middleware de CORS

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const path = require('path')
+const cors = require('cors')
 
 const db = require('./database/db')
 const routes = require('./routes/routes')
@@ -8,6 +8,27 @@ const app = express()
 
 //conexão com o  banco de dados
 db.connect()
+
+const allowedOrigins = [
+    'http://127.0.0.1:5500',
+    'http://www.app.com.br'
+]
+//habilita cors
+//app.use(cors()) --> aqui a api fica publica
+app.use(cors({
+    // origin:'http://127.0.0.1:5500'
+    origin: function(origin, callback){
+        let allowed = true
+
+        //mobile app
+        if(!origin) allowed = true
+
+        if(!allowedOrigins.includes(origin)) allowed = false
+
+        //no lugar do null é uma mensagem
+        callback(null, allowed)
+    }
+}))
 
 //habilita server para receber dados json
 app.use(express.json())
